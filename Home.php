a80ca0e66e9a2d56cf0fa14a764a5c536e2c79d8
Mika <t8ojmi00@students.oamk.fi>
2019-09-18 16:00:16 +0300

    Html runko Home luotu

---
+++ Home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div id="Home">
+      Home
+    </div>
+    <div id="Tuoteryhma">
+      Tuoteryhmät
+    </div>
+    <div id="Kirjaudu">
+      Kirjaudu sisään
+    </div>
+    <div id="Yhteystiedot">
+      Yhteystiedot
+    </div>
+  </body>
+</html>
