b5d4d818204b1e18dde0c7fe0853f279bbcd6b78
Mika Ojanlatva <t8ojmi00@students.oamk.fi>
2019-09-28 12:16:48 +0300

    Palautekenttä luotu

--- post.php
+++ post.php
@@ -3,4 +3,15 @@
 	<p>
 		This is the Contact site.
 	</p>
+	<p>
+		<br>
+		<b>Lähetä palautetta</b> <br>
+				<textarea name="name" rows="8" cols="80"></textarea>
+				<br>
+				<input type="submit" name="" value="Lähetä">
+				<br>
+				<br>
+				<input type="date" name="pvm" value="" min=1900>
+	</p>
+
 <?php include "footer.php"; ?>
